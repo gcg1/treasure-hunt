9ae2389a43eb63c02a082589c8c7ceecfd6f8d17
George <georgefbcunliffe@gmail.com>
2020-08-31 18:27:53 +0100

    Updates

---
+++ precache-manifest.8f78d3caee7da90e40498c5e3bd20f78.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072e336bf37f27df629933c37dde4f88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bd08d336e28998d991a",
+    "url": "/static/css/2.9d655fbe.chunk.css"
+  },
+  {
+    "revision": "827de83ecea209a435a7",
+    "url": "/static/css/main.3ed82a94.chunk.css"
+  },
+  {
+    "revision": "9bd08d336e28998d991a",
+    "url": "/static/js/2.fa931464.chunk.js"
+  },
+  {
+    "revision": "a9a88137380ab0bdb96073236cf03ba2",
+    "url": "/static/js/2.fa931464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "827de83ecea209a435a7",
+    "url": "/static/js/main.0b4c03b0.chunk.js"
+  },
+  {
+    "revision": "e32e76728290f9169519",
+    "url": "/static/js/runtime-main.2ef6263e.js"
+  },
+  {
+    "revision": "25d5fb55628b86b8a752525e18da3342",
+    "url": "/static/media/GloriaHallelujah.25d5fb55.ttf"
+  },
+  {
+    "revision": "8fc622c3a2e2d992ec059cca61e3dfc0",
+    "url": "/static/media/Impact.8fc622c3.ttf"
+  },
+  {
+    "revision": "0c8af0bc1b2268f8777fbcb3d010c4bb",
+    "url": "/static/media/creak.0c8af0bc.mp3"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "e684d7000643a195caa82849b3076917",
+    "url": "/static/media/fanfare.e684d700.mp3"
+  },
+  {
+    "revision": "1249411f4f9bb9d1aa8c05f6f024cf16",
+    "url": "/static/media/locked.1249411f.mp3"
+  },
+  {
+    "revision": "5439ac1659df0ef81686db2d489fe1d1",
+    "url": "/static/media/map.5439ac16.svg"
+  },
+  {
+    "revision": "3e7e80656d8578f2162e173005d03e9f",
+    "url": "/static/media/ohdear.3e7e8065.mp3"
+  },
+  {
+    "revision": "ce103ba4096dffa85297e5f9bb5aeed2",
+    "url": "/static/media/staticChest.ce103ba4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2d89e2cf408a0dc18c7e80a77d063245.js"
+  "/precache-manifest.8f78d3caee7da90e40498c5e3bd20f78.js"
 );
 
 self.addEventListener('message', (event) => {
